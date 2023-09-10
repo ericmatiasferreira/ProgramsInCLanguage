1414f77da180cdeae4a63d1cfce3367cfd310f72
Eric Matias <131385064+ELBRASILE@users.noreply.github.com>
2023-09-09 21:55:57 -0300

    Create APII_Lista04_Questão1.c

---
+++ APII_Lista04_Questão1.c
@@ -0,0 +1,15 @@
+/* 
+      Eric Matias Ferreira Dias. 
+      Uiversidade Estadual De Goias 
+      Programa que efetua uma pesquisa binaria.
+      Visite meu GitHub em https://github.com/ELBRASILE
+
+      1 – Faça um programa que receba uma frase (string), substitua todas as vogais pelo
+      caractere * e converte todas as consoantes para maiúsculo.
+      Exemplo:
+      Entrada: fazer exercicios faz bem.
+      Saída: F*Z*R *X*RC*C**S F*Z B*M.
+
+      
+*/
+
