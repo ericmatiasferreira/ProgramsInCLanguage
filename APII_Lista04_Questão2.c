bad9539037e8573f6825d5e2f82040701521e3f6
Eric Matias <131385064+ELBRASILE@users.noreply.github.com>
2023-09-11 09:05:01 -0300

    Create  APII_Lista04_Questão2.c

---
+++ APII_Lista04_Questão2.c
@@ -0,0 +1,35 @@
+/*
+Eric Matias Ferreira Dias 
+Universidade Estadual De Goias 
+
+2 - Faça um programa que receba uma frase e substitua todas as consoantes pelo
+caractere @.
+Exemplo:
+Entrada: LOGICA DE PROGRAMAÇAO.
+Saída: @O@I@A @E @@O@@A@A@AO
+
+
+
+*/
+#define T 100; 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h> 
+int main()
+{
+  char Frase[T]; 
+  int I = 0; 
+  printf("Entrada: ");
+  fgets(Frase, sizeof(Frase), stdin);
+  while(Frase[I] != '\0')
+  {
+    if(isalpha(Frase[I]) && Frase[I] != 'a' && Frase[I] != 'e' && Frase[I] != 'i' && Frase[I] != 'o' && Frase[I] != 'u' 
+        && Frase[I] != 'A' && Frase[I] != 'E' && Frase[I] != 'I' && Frase[I] != 'O' && Frase[I] != 'U')
+        {
+            Frase[I] = "@"; 
+        }
+  }
+  printf("%s",Frase); 
+  return 0; 
+}
