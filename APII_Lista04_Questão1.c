dd23fd3ef92436e9876db920e68dacf3ab02c0b5
Eric Matias <131385064+ELBRASILE@users.noreply.github.com>
2023-09-11 08:57:57 -0300

    Update APII_Lista04_Questão1.c

--- APII_Lista04_Questão1.c
+++ APII_Lista04_Questão1.c
@@ -9,7 +9,35 @@
       Exemplo:
       Entrada: fazer exercicios faz bem.
       Saída: F*Z*R *X*RC*C**S F*Z B*M.
-
-      
 */
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <ctype.h>
+int main(void)
+{
+    char Frase[100]; 
+    int I=0; 
+    printf("Entrada: "); 
+    fgets(Frase, sizeof(Frase), stdin ); 
+    while(Frase[I] != '\0')
+    {
+        if(Frase[I] == 'a' || Frase[I] == 'e' || Frase[I] == 'i' || Frase[I] == 'o' || Frase[I] == 'u' 
+        || Frase[I] == 'A' || Frase[I] == 'E' || Frase[I] == 'I' ||Frase[I] == 'O' || Frase[I] == 'U')
+        {
+            Frase[I] = '*';
+        }
+        
+        if(isalpha(Frase[I]) && Frase[I] != 'a' && Frase[I] != 'e' && Frase[I] != 'i' && Frase[I] != 'o' && Frase[I] != 'u' 
+        && Frase[I] != 'A' && Frase[I] != 'E' && Frase[I] != 'I' && Frase[I] != 'O' && Frase[I] != 'U')
+        {
+            Frase[I] = toupper(Frase[I]); 
+        }
+        
+        I++;
+    }
+    printf("Saida: "); 
+    printf("%s",Frase); 
+    //system("pause");
+    return 0; 
+}
