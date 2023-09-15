23302e415d8a5c1e17f9a8afb93e83bfa53435f4
Eric Matias <131385064+ELBRASILE@users.noreply.github.com>
2023-09-15 20:56:50 -0300

    Update APII_Lista04_Questão5.c
    Versão com uma lógica mais inteligente, precisa e direta de entender.

--- APII_Lista04_Questão5.c
+++ APII_Lista04_Questão5.c
@@ -5,10 +5,14 @@ Universidade Estadual De Goias
 5 - Faça um programa que leia um email e faça sua validação. Se não for um email
 válido o programa deve mostrar a mensagem: “Email invalido!” e pedir para ler
 novamente.
+
+
 Plano de Teste
+
 Exemplos de Entrada Exemplos de Saída
 teste@ueg.br
 E-mail válido.
+
 www.adms.ueg.br
 si.ccet@ueg.br
 E-mail inválido. Digite novamente:
@@ -20,36 +24,30 @@ E-mail inválido. Digite novamente:
 E-mail inválido. Digite novamente:
 E-mail válido.
 */
+
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define T 50
 
 int main(void)
 {
-    char Email[T];
-    int Cumbuca = 0;
-
-    while (Cumbuca == 0)
+    char Email[T]; 
+    bool Validacao = false; 
+    do 
     {
-        printf("Digite um e-mail: ");
-        fgets(Email, sizeof(Email), stdin);
-
-        // Remova o caractere de nova linha (se presente) da entrada do usuário
-        size_t len = strlen(Email);
-        if (len > 0 && Email[len - 1] == '\n')
+        fgets(Email,sizeof(Email),stdin);  
+        
+        if((strchr(Email, '@') != NULL) && strstr(Email, "ueg.br")) 
+            // Alterar este se quiser outro dominio. Ex aluno@gmail.com 
+            break;   
+        else
         {
-            Email[len - 1] = '\0';
+            printf("E-mail inválido. Digite novamente:\n"); 
+            fgets(Email,sizeof(Email),stdin);  
         }
-
-        if (strchr(Email, '@') != NULL)
-        {
-            Cumbuca = 1;
-        }
-
-        printf(Cumbuca == 1 ? "E-mail válido.\n" : "E-mail inválido. Digite novamente.\n");
-    }
-
-    return 0;
+        
+    }while(Validacao == false); 
+    printf("E-mail válido.\n"); 
 }
-
