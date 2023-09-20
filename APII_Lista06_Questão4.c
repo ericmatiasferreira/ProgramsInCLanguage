566ea5c940653035013d61a495a9f1652e66a99c
Eric Matias <131385064+ELBRASILE@users.noreply.github.com>
2023-09-19 22:21:15 -0300

    APII_Lista06_Questão4.c

---
+++ APII_Lista06_Questão4.c
@@ -0,0 +1,34 @@
+/*
+
+Eric Matias Ferreira Dias 
+Universidade Estadual de Goiás 
+Sistemas De Informação 
+4° Período. 
+
+/*
+    4 - Faça um programa que leia uma matriz 3x5 de números inteiros, depois leia uma
+    variável inteira que representa uma coluna da matriz e escreva todas as linhas desta
+    coluna.
+
+*/
+
+
+
+#include <stdio.h>
+#include <locale.h>
+
+#define LINE 3 
+#define COLUMN 5 
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+    
+    
+    int Matriz[LINE][COLUMN],I,J; 
+    
+    
+    
+    
+    return 0;
+}
